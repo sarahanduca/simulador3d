2b837c1ff94d1dd45f640dc88736ed27ff629234
Sarah Anduca <sarah.anduca@eurekalabs.com.br>
2022-05-22 20:59:50 -0300

    first commit

--- simulador3d.py
+++ simulador3d.py
@@ -1,185 +1,76 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-import pygame
+from scipy.spatial import ConvexHull
 import numpy as np
 from math import *
 
-WHITE = (255, 255, 255)
-RED = (0, 0, 0)
-BLACK = (0, 255, 0)
-
-WIDTH, HEIGHT = 800, 600
-pygame.display.set_caption("3D projection in pygame!")
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-scale = 100
-
-circle_pos = [WIDTH/2, HEIGHT/2]  # x, y
-
-angle = 0
-
 cubeMatrix = np.matrix([[1,7, 7, 1, 4], [1, 1, 1, 1, 7], [1, 1, 7, 7, 4], [1, 1, 1, 1, 1]])
-points = [["3", "2", "1"], ["1", "2", "1"], ["2", "1", "3"]]
-pov = ["4", "5", "7"]
+points = [["0", "1", "0"], ["1", "0", "0"], ["0", "0", "0"]]
+pov = ["20", "10", "30"]
 # pov = input("Primeiro ponto de vista: ").split(",")
 # for i in range(3):
 #     points.append(input("Digite um ponto do plano: ").split(","))
 
 
-def calcN(points):
-    p1 = points[0]
-    p2 =  points[1]
-    p3 = points[2]
-    nx = (int(p1[1]) * int(p2[1])) *  (int(p3[2]) * int(p2[2])) - ((int(p3[1]) * int(p2[1])) * (int(p1[2]) * int(p2[2])))
-    ny = -1 * (int(p1[0]) * int(p2[0])) *  (int(p3[2]) * int(p2[2])) - ((int(p3[0]) * int(p2[0])) * (int(p1[2]) * int(p2[2])))
-    nz = (int(p1[0]) * int(p2[0])) *  (int(p3[1]) * int(p2[1])) - ((int(p3[0]) * int(p2[0])) * (int(p1[1]) * int(p2[1])))
-
-    return nx, ny, nz
+# def calcN(points):
+#     p1 = points[0]
+#     p2 =  points[1]
+#     p3 = points[2]
+#     nx = (int(p1[1]) * int(p2[1])) *  (int(p3[2]) * int(p2[2])) - ((int(p3[1]) * int(p2[1])) * (int(p1[2]) * int(p2[2])))
+#     ny = -1 * (int(p1[0]) * int(p2[0])) *  (int(p3[2]) * int(p2[2])) - ((int(p3[0]) * int(p2[0])) * (int(p1[2]) * int(p2[2])))
+#     nz = (int(p1[0]) * int(p2[0])) *  (int(p3[1]) * int(p2[1])) - ((int(p3[0]) * int(p2[0])) * (int(p1[1]) * int(p2[1])))
+#     return nx, ny, nz
 
 def calcD(pov, points):
-    nx, ny, nz = calcN(points)
+    # nx, ny, nz = calcN(points)
+    nx, ny, nz = 0, 0, 1
     a, b, c = int(pov[0]), int(pov[1]), int(pov[2])
     point = points[0]
 
     d0 = (int(point[0])*nx) + (int(point[1]) * ny) + (int(point[2]) * nz)
     d1 = (a * nx) + (b * ny) + (c * nz)
+    d = d0 - d1
 
-    return d0 - d1, d0
+    return d, d0, d1
 
 def buildMatrix(pov, points):
-    nx, ny, nz = calcN(points)
-    d, d0 = calcD(pov, points)
+    # nx, ny, nz = calcN(points)
+    nx, ny, nz = 0, 0, 1
+
+    d, d0, d1 = calcD(pov, points)
     a, b, c = int(pov[0]), int(pov[1]), int(pov[2])
     return np.matrix([[d + a*nx, a*ny, a*nz, -1*a*d0], 
     [b*nx, d+b*ny, b*nz, -1*b*d0], 
     [c*nx, c*ny, d+c*nz, -1*c*d0],
-    [nx, ny, nz, 1]])
+    [nx, ny, nz, -d1]])
 
 def finalMatrix(objectMatrix):
     w = objectMatrix[3].getA1()
     line1 = objectMatrix[0].getA1()
     line2 = objectMatrix[1].getA1()
     line3 = objectMatrix[2].getA1()
-    finalMatrix = np.matrix([
-        [line1[0]/w[0], line1[1]/w[1], line1[2]/w[2], line1[3]/w[3]],
-        [line2[0]/w[0], line2[1]/w[1], line2[2]/w[2], line2[3]/w[3]],
-        [line3[0]/w[0], line3[1]/w[1], line3[2]/w[2], line3[3]/w[3]]
+    return np.matrix([
+        [line1[0]/w[0], line1[1]/w[1], line1[2]/w[2], line1[3]/w[3], line1[4]/w[4]],
+        [line2[0]/w[0], line2[1]/w[1], line2[2]/w[2], line2[3]/w[3], line1[4]/w[4]],
+        [line3[0]/w[0], line3[1]/w[1], line3[2]/w[2], line3[3]/w[3], line1[4]/w[4]]
     ])
-    return finalMatrix.getT()
-    
-objectMatrix = buildMatrix(pov, points) * cubeMatrix
-finalMatrix = finalMatrix(objectMatrix)
 
-# all the cube vertices
-displayVertex = []
-displayVertex.append(np.matrix(finalMatrix[0].getA1()))
-displayVertex.append(np.matrix(finalMatrix[1].getA1()))
-displayVertex.append(np.matrix(finalMatrix[2].getA1()))
-displayVertex.append(np.matrix(finalMatrix[3].getA1()))
+def main():
+    print(finalMatrix)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
 
+    X, Y, Z = finalMatrix[0].getA1(), finalMatrix[1].getA1(), finalMatrix[2].getA1()
 
+    ax.plot(X, Y, Z, "ro")
 
-# fig = plt.figure()
+    hull = ConvexHull(np.array([X, Y, Z]).T)
+    for s in hull.simplices:
+        s = np.append(s, s[0])
+        ax.plot(X[s], Y[s], Z[s], "b-")
+    plt.show()
 
-# ax = fig.add_subplot(111, projection='3d')
-
-# x = [1,7, 7, 1, 4]
-
-# y = [1, 1, 1, 1, 7]
-
-# z = [1, 1, 7, 7, 4]
-
-# vertices = [list(zip(x,y,z))]
-
-# poly = Poly3DCollection(vertices, alpha=0.6)
-
-# ax.add_collection3d(poly)
-
-# ax.plot_trisurf(x, y, z,
-#                 cmap='viridis', edgecolor='none');
-# # ax.set_xlim(0,5)
-
-# # ax.set_ylim(0,5)
-
-# # ax.set_zlim(0,5) 
-
-# plt.show()
-
-
-# print(points)
-
-projection_matrix = np.matrix([
-    [1, 0, 0],
-    [0, 1, 0]
-])
-
-
-projected_points = [
-    [n, n] for n in range(len(displayVertex))
-]
-
-
-def connect_points(i, j, points):
-    pygame.draw.line(
-        screen, BLACK, (points[i][0], points[i][1]), (points[j][0], points[j][1]))
-
-
-clock = pygame.time.Clock()
-while True:
-
-    clock.tick(60)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                exit()
-
-    # update stuff
-
-    # rotation_z = np.matrix([
-    #     [cos(angle), -sin(angle), 0],
-    #     [sin(angle), cos(angle), 0],
-    #     [0, 0, 1],
-    # ])
-
-    # rotation_y = np.matrix([
-    #     [cos(angle), 0, sin(angle)],
-    #     [0, 1, 0],
-    #     [-sin(angle), 0, cos(angle)],
-    # ])
-
-    # rotation_x = np.matrix([
-    #     [1, 0, 0],
-    #     [0, cos(angle), -sin(angle)],
-    #     [0, sin(angle), cos(angle)],
-    # ])
-    # angle += 0.01
-
-    screen.fill(WHITE)
-#     # drawining stuff
-
-    i = 0
-    for point in displayVertex:
-        # rotated2d = np.dot(rotation_z, point.reshape((3, 1)))
-        # rotated2d = np.dot(rotation_y, rotated2d)
-        # rotated2d = np.dot(rotation_x, rotated2d)
-
-        projected2d = np.dot(projection_matrix)
-
-        x = int(projected2d[0][0] * scale) + circle_pos[0]
-        y = int(projected2d[1][0] * scale) + circle_pos[1]
-
-        projected_points[i] = [x, y]
-        pygame.draw.circle(screen, RED, (x, y), 5)
-        i += 1
-
-    for p in range(4):
-        connect_points(p, (p+1) % 4, projected_points)
-        connect_points(p+4, ((p+1) % 4) + 4, projected_points)
-        connect_points(p, (p+4), projected_points)
-
-    pygame.display.update()
+if __name__ == '__main__':
+    objectMatrix = buildMatrix(pov, points) * cubeMatrix
+    print(buildMatrix(pov, points))
+    finalMatrix = finalMatrix(objectMatrix)
+    main()
